af526e779497da44ea0f6624325df30b3877058c
Farruxbey <90433252+Farruxbey@users.noreply.github.com>
2021-09-24 08:50:06 +0500

    Add files via upload

---
+++ src/config/development.example.js
@@ -0,0 +1,9 @@
+module.exports = {
+  token: '2024505396:AAHK6HF1S-aA7CEfT23vCeXKJ2XpPb9hWV0',
+  botAdmin: '765666917',
+  mongodb: {
+    url: 'mongodb+srv://mongodb-url',
+    database: 'DB_NAME',
+  },
+  allowSamePlayer: true,
+};
